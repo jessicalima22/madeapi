91debf7110011826084df376af48e816a4e1cc1b
jessicalima22 <jessica.lima22@hotmail.com>
2023-06-16 00:19:36 -0300

    trying function

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -1,27 +1,25 @@
 import axios from "axios";
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-export default class carList extends React.Component {
- state={
-    cars:[]
- }
 
- componentDidMount(){
+export default function carList(){
+ const [cars, setCars]=useState([])
+
+ useEffect(()=>{
     axios.get('https://1stapireact.jessicalima22.repl.co')
         .then(res=>{
             const dataCars = res.data
-            this.setState({cars: dataCars})
+            setCars(dataCars)
         })
- }
+ })
 
- render(){
     return(
         <div>
-            {this.state.cars.map(
+            {cars.map(
                 car=> <div key={car.id}>{car.brand} - {car.model} </div>
             )}
         </div>
     )
- }
+ 
 
 }
