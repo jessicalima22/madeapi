b1d8b90f03d25a6389864e47429817e6fbe9352a
jessicalima22 <jessica.lima22@hotmail.com>
2023-06-18 23:52:32 -0300

    cors solved

--- components/CarList.jsx
+++ components/CarList.jsx
@@ -4,25 +4,24 @@ import React, {useState, useEffect} from "react";
 export default function carList(){
  const [cars, setCars]=useState([])
 
- useEffect(()=>{
-
-    fetch('https://1stapireact.jessicalima22.repl.co',{
-        mode: "no-cors",
+     useEffect(()=>{
+        fetch ('https://cors-anywhere.herokuapp.com/https://1stapireact.jessicalima22.repl.co')
+            .then(res=>res.json())
+            .then(
+                (result)=>{
+                    setCars(result)
+                }
+            )
     })
-    .then((res)=>res.json())
-    .then(
-        (result)=>{
-            setCars(result)
-        }
-    )
-
- })
-
+        
+     
     return(
         <div>
+
+            <h1>Calling API via REPL</h1>
             {cars.map(
                 car=> <div key={car.id}>{car.brand} - {car.model} </div>
-            )}
+            )}  
         </div>
     )
  
